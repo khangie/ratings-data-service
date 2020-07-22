15a3c2662efd71ccb0c791ef8a76b91d2dfbe938
= <khangie@hotmail.com>
2020-07-21 21:15:12 -0700

    new updates

--- src/main/java/io/javabrains/moviecatalogservice/resource/RatingsResource.java
+++ src/main/java/io/javabrains/moviecatalogservice/resource/RatingsResource.java
@@ -1,9 +1,14 @@
 package io.javabrains.moviecatalogservice.resource;
 
 import io.javabrains.moviecatalogservice.models.Rating;
+import io.javabrains.moviecatalogservice.models.UserRating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/ratingsdata")
@@ -12,5 +17,14 @@ public class RatingsResource {
     @RequestMapping("/{movieId}")
     public Rating getRating(@PathVariable("movieId") String movieId) {
         return new Rating(movieId, 4);
+    }
+
+    @RequestMapping("/users/{userId}")
+    public UserRating getUserRating(@PathVariable("userId") String userId) {
+        List<Rating> ratings = Arrays.asList(
+            new Rating("1234", 4),
+            new Rating("5678", 3)
+        );
+        return new UserRating(ratings);
     }
 }
